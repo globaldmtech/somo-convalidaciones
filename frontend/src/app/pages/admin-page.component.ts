957c64cd66616657b3664af38f06a10684465cda
janire.amesti@globaldataquantum.com <janire.amesti@globaldataquantum.com>
2026-03-12 12:13:35 +0100

    cambio simbolo archivo

--- frontend/src/app/pages/admin-page.component.ts
+++ frontend/src/app/pages/admin-page.component.ts
@@ -807,11 +807,8 @@ type PendingConvalidacionOrigenDelete = {
                           [disabled]="isFormularioUpdating(f.id)"
                           (click)="solicitarCambioEstadoFormulario(f.id, alumno.nombre, getArchiveEstadoObjetivo(f.estado_id), f.estado_id)"
                         >
-                          <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-1 11a2 2 0 01-2 2H7a2 2 0 01-2-2L4 7"></path>
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 10h6"></path>
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7h18"></path>
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 4h4"></path>
+                          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true">
+                            <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5zm13-3H1v2h14zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
                           </svg>
                           Archivar
                         </button>
